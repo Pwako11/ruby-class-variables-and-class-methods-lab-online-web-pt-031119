396d5f9b63ba8f778842090287a2a38dc8594a89
Patrick A <akolo@hotmail.com>
2019-04-01 00:40:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,23 @@ class Song
   
   attr_accessor :artist, :genre, :name 
   
+  @@count = 0 
+  @@genres = []
+  
   def initialize (name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre
+    @@count += 1
+    @@genres += 1
   end 
   
+  def self.count
+    @@count
+  end 
+  
+  def self.genres(genre)
+    
+    
+  end 
 end 
